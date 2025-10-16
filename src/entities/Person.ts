57d98bc930678f47f437d35023e5381c91e4d974
Wei Yang <wei0038@proton.me>
2025-10-16 12:10:52 +0200

    tested json utf-8 and change person to a class for template

--- src/entities/Person.ts
+++ src/entities/Person.ts
@@ -1,14 +1,33 @@
-export enum GENDER {
-  male = "male",
-  female = "female",
-}
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-export interface Person {
-  name: string;
-  surname: string;
-  gender: GENDER
-  cpr: string;
-  address: Address;
-  phone: string;
-  birthdate: Date;
+export type TGender = "male" | "female";
+
+@Entity("person")
+export class Person {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  surname!: string;
+
+  @Column({
+    type: "enum",
+    enum: ["male", "female"],
+  })
+  gender!: TGender;
+
+  @Column({ unique: true })
+  cpr!: string;
+
+  @Column()
+  address!: string;
+
+  @Column()
+  phone!: string;
+
+  @Column({ type: "date" })
+  birthdate!: Date;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import personRouter from "./routes/personRouter.js";
+import fs from "fs";
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.use(express.json());
 app.use("/api/person", personRouter);
 
 const PORT = 8080;
+
+const file = fs.readFileSync("persons.json").toString();
+const personNames = JSON.parse(file);
+
+console.log("File:");
+console.log(personNames);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
