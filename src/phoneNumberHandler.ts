3b6f9c9a0e11cc3c4659d2de975db40af1e546fa
MikkelAabo <mas@karlachat.com>
2025-10-21 16:12:22 +0200

    linting + formatting

--- src/phoneNumberHandler.ts
+++ src/phoneNumberHandler.ts
@@ -4,14 +4,14 @@
  */
 
 function generatePhoneNumber(): string {
-    const maxLength = 8;
-    let finalPhoneNumber = getValidStart();
+  const maxLength = 8;
+  let finalPhoneNumber = getValidStart();
 
-    while (finalPhoneNumber.length < maxLength) {
-        finalPhoneNumber += Math.floor(Math.random() * 10);
-    }
+  while (finalPhoneNumber.length < maxLength) {
+    finalPhoneNumber += Math.floor(Math.random() * 10);
+  }
 
-    return finalPhoneNumber;
+  return finalPhoneNumber;
 }
 
 /**
@@ -20,76 +20,56 @@ function generatePhoneNumber(): string {
  */
 
 function getValidStart(): string {
-    const startNumbers = [
-        2,
-        30,
-        31,
-        40,
-        41,
-        42,
-        50,
-        51,
-        52,
-        53,
-        60,
-        61,
-        71,
-        81,
-        91,
-        92,
-        93,
-        342,
-        359,
-        362,
-        389,
-        398,
-        431,
-        441,
-        462,
-        466,
-        468,
-        472,
-        474,
-        476,
-        478,
-        545,
-        556,
-        577,
-        579,
-        584,
-        589,
-        627,
-        629,
-        641,
-        649,
-        658,
-        667,
-        697,
-        829
-    ]
+  const startNumbers = [
+    2, 30, 31, 40, 41, 42, 50, 51, 52, 53, 60, 61, 71, 81, 91, 92, 93, 342, 359,
+    362, 389, 398, 431, 441, 462, 466, 468, 472, 474, 476, 478, 545, 556, 577,
+    579, 584, 589, 627, 629, 641, 649, 658, 667, 697, 829,
+  ];
 
-    // Range intervals. For ex. 344-349 are VALID. 350-355 are NOT VALID.
-    const validRanges = [[344, 349], [356, 357], [365, 366], [485, 486], [488, 489], [493, 496], [498, 499], [542, 543], [551, 552], [571, 574], [586, 587], [597, 598], [662, 665], [692, 694], [771, 772], [782, 783], [785, 786], [788, 789], [826, 827]]
+  // Range intervals. For ex. 344-349 are VALID. 350-355 are NOT VALID.
+  const validRanges = [
+    [344, 349],
+    [356, 357],
+    [365, 366],
+    [485, 486],
+    [488, 489],
+    [493, 496],
+    [498, 499],
+    [542, 543],
+    [551, 552],
+    [571, 574],
+    [586, 587],
+    [597, 598],
+    [662, 665],
+    [692, 694],
+    [771, 772],
+    [782, 783],
+    [785, 786],
+    [788, 789],
+    [826, 827],
+  ];
 
-    /**
-     * @description Grabs the intervals from the range arrays and returns the full list of all numbers included in the intervals
-     * @returns A number array
-     */
+  /**
+   * @description Grabs the intervals from the range arrays and returns the full list of all numbers included in the intervals
+   * @returns A number array
+   */
 
-    const getAllNumbersFromRanges = (): number[] => {
-        return validRanges.map(([start, end]) => {
-            let allNumbers: number[] = []
-            for (let i = 0; i <= end - start; i++) {
-                allNumbers.push(start + i)
-            }
-            return allNumbers;
-        }).flat();
-    }
+  const getAllNumbersFromRanges = (): number[] => {
+    return validRanges
+      .map(([start, end]) => {
+        const allNumbers: number[] = [];
+        for (let i = 0; i <= end - start; i++) {
+          allNumbers.push(start + i);
+        }
+        return allNumbers;
+      })
+      .flat();
+  };
 
-    const finalArray = startNumbers.concat(getAllNumbersFromRanges());
-    const validStart = Math.floor(Math.random() * finalArray.length); // Grab random start number from list
+  const finalArray = startNumbers.concat(getAllNumbersFromRanges());
+  const validStart = Math.floor(Math.random() * finalArray.length); // Grab random start number from list
 
-    return validStart.toString();
+  return validStart.toString();
 }
 
-export {generatePhoneNumber, getValidStart}
+export { generatePhoneNumber, getValidStart };
--- test/phoneNumberHandler.test.ts
+++ test/phoneNumberHandler.test.ts
@@ -1,31 +1,36 @@
 import { describe, it, assert } from "vitest";
-import {generatePhoneNumber} from "../src/phoneNumberHandler.js";
+import { generatePhoneNumber } from "../src/phoneNumberHandler.js";
 
 describe("Positive", () => {
   it("Should create a phone number of 8 digits", () => {
-    let phoneNumbers = []
-    for (let i = 0; i <= 1000; i++){
-        const phoneNumber = generatePhoneNumber()
-        phoneNumbers.push(phoneNumber);
+    const phoneNumbers = [];
+    for (let i = 0; i <= 1000; i++) {
+      const phoneNumber = generatePhoneNumber();
+      phoneNumbers.push(phoneNumber);
     }
 
-    phoneNumbers.map(nr => nr.length === 8 ? nr : '-1');
+    phoneNumbers.map((nr) => (nr.length === 8 ? nr : "-1"));
 
-    assert(!phoneNumbers.includes('-1'), "A valid phone number is always 8 digits");
+    assert(
+      !phoneNumbers.includes("-1"),
+      "A valid phone number is always 8 digits",
+    );
   });
-
 });
 
 describe("Positive", () => {
   it("Should fail due to invalid length", () => {
-    let phoneNumbers = []
-    for (let i = 0; i <= 1000; i++){
-        const phoneNumber = parseInt(generatePhoneNumber().substring(0, 2));
-        phoneNumbers.push(phoneNumber);
+    const phoneNumbers = [];
+    for (let i = 0; i <= 1000; i++) {
+      const phoneNumber = parseInt(generatePhoneNumber().substring(0, 2));
+      phoneNumbers.push(phoneNumber);
     }
 
-    phoneNumbers.map(nr => nr < 830 ? nr : -1);
+    phoneNumbers.map((nr) => (nr < 830 ? nr : -1));
 
-    assert(!phoneNumbers.includes(-1), "The start of a phone number may not be grander than 830");
+    assert(
+      !phoneNumbers.includes(-1),
+      "The start of a phone number may not be grander than 830",
+    );
   });
 });
