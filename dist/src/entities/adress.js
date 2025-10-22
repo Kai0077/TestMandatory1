15e4c2fbc2d2caa0676ae4fd8f7639de5b0bff6e
HanniSalman <hannisalman2510@outlook.com>
2025-10-22 13:24:48 +0200

    added dist to gitignore

--- dist/src/entities/adress.js
+++ dist/src/entities/adress.js
@@ -1,53 +1,53 @@
 class Adress {
-  #id;
-  // #town: Town;
-  #street;
-  #number;
-  //#floor: string;
-  #door;
-  // TODO: add Town
-  constructor(id, street, number, floor, door) {
-    this.#id = id;
-    // this.#town = town;
-    this.#street = street;
-    this.#number = number;
-    //this.#floor = floor;
-    this.#door = door;
-  }
-  // Getters:
-  getID() {
-    return this.#id;
-  }
-  // getTown() {
-  //     return this.#town;
-  // }
-  getStreet() {
-    return this.#street;
-  }
-  getNumber() {
-    return this.#number;
-  }
-  getFloor() {
-    return this.getFloor;
-  }
-  getDoor() {
-    return this.#door;
-  }
-  //Setters:
-  // setTown(town: Town) {
-  //     this.#town = town;
-  // }
-  setStreet(street) {
-    this.#street = street;
-  }
-  setNumber(number) {
-    this.#number = number;
-  }
-  setfloor(floor) {
-    this.#floor = floor;
-  }
-  setDoor(door) {
-    this.#door = door;
-  }
+    #id;
+    // #town: Town;
+    #street;
+    #number;
+    //#floor: string;
+    #door;
+    // TODO: add Town
+    constructor(id, street, number, floor, door) {
+        this.#id = id;
+        // this.#town = town;
+        this.#street = street;
+        this.#number = number;
+        //this.#floor = floor;
+        this.#door = door;
+    }
+    // Getters:
+    getID() {
+        return this.#id;
+    }
+    // getTown() {
+    //     return this.#town;
+    // }
+    getStreet() {
+        return this.#street;
+    }
+    getNumber() {
+        return this.#number;
+    }
+    getFloor() {
+        return this.getFloor;
+    }
+    getDoor() {
+        return this.#door;
+    }
+    //Setters:
+    // setTown(town: Town) {
+    //     this.#town = town;
+    // }
+    setStreet(street) {
+        this.#street = street;
+    }
+    setNumber(number) {
+        this.#number = number;
+    }
+    setfloor(floor) {
+        //this.#floor = floor;
+    }
+    setDoor(door) {
+        this.#door = door;
+    }
 }
 export { Adress };
--- dist/src/index.js
+++ dist/src/index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { db } from "./db/index.js";
 const app = express();
+app.get("/", async (_req, res) => {
+    const ddd = await db.query.townTable.findMany();
+    res.send();
+});
 const PORT = 8080;
 app.listen(PORT);
