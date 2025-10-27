984face50556b1655a4f66ad459b4b646c0bf985
Wei Yang <wei0038@proton.me>
2025-10-27 10:46:11 +0100

    removed dist folder

--- dist/src/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import personRouter from "./personRouter.js";
-const router = express.Router();
-router.use(express.json());
-router.use("/person", personRouter);
-export default router;
