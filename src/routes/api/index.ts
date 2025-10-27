7fb2d3dc0b48059dca97a75f1b082e873f09876d
Wei Yang <wei0038@proton.me>
2025-10-27 11:08:49 +0100

    removed idea folder and added endpoint to frontend to .env

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -3,6 +3,10 @@ import personRouter from "./personRouter.js";
 
 const router = express.Router();
 
+router.get("/config", (req, res) => {
+    res.status(200).json({ baseUrl: process.env.BASE_URL || "" });
+});
+
 router.use(express.json());
 
 router.use("/person", personRouter);
