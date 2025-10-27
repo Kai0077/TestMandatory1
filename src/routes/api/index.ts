80fa7f0407f8265897f7ceff12991464d9a5d57c
Wei Yang <wei0038@proton.me>
2025-10-27 11:11:19 +0100

    fix: format

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -4,7 +4,7 @@ import personRouter from "./personRouter.js";
 const router = express.Router();
 
 router.get("/config", (req, res) => {
-    res.status(200).json({ baseUrl: process.env.BASE_URL || "" });
+  res.status(200).json({ baseUrl: process.env.BASE_URL || "" });
 });
 
 router.use(express.json());
