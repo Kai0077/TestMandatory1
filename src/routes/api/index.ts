e30f736e174406a6de7de9bdd70f07de00fc04bc
doro-hd <doro-hd@protonmail.com>
2025-10-27 11:32:01 +0100

    Remove config

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -3,10 +3,6 @@ import personRouter from "./personRouter.js";
 
 const router = express.Router();
 
-router.get("/config", (req, res) => {
-  res.status(200).json({ baseUrl: process.env.BASE_URL || "" });
-});
-
 router.use(express.json());
 
 router.use("/person", personRouter);
